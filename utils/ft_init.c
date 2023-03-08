59f8efce925522fbabeccc22b804ff7ecbc49c54
rinacio <revazgauska@gmail.com>
2023-03-08 03:44:01 +0100

    token struct

--- utils/ft_init.c
+++ utils/ft_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_init.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rinacio <rinacio@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rinacio <rinacio@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:07:07 by rferrero          #+#    #+#             */
-/*   Updated: 2023/03/07 19:13:08 by rinacio          ###   ########.fr       */
+/*   Updated: 2023/03/08 01:57:24 by rinacio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void    ft_init(t_program *g_data)
     //g_data->cmd_token =  ft_strdup("");
     // g_data->env =  NULL;
     // g_data->path =  NULL;
+	g_data->token_start = NULL;
     g_data->status = 0;
     g_data->stop = 0;
 }
---
+++ utils/token_list.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_list.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rinacio <rinacio@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 01:53:47 by rinacio           #+#    #+#             */
+/*   Updated: 2023/03/08 03:29:40 by rinacio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	ft_add_token(t_program *g_data, char *cmd_token, t_type cmd_type)
+{
+	t_token	*new_token;
+	t_token	*aux;
+	
+	new_token = (t_token*)malloc(sizeof(t_token));
+	new_token->cmd = cmd_token;
+	new_token->type = cmd_type;
+	new_token->prev = NULL;
+	new_token->next = NULL;
+	if (!g_data->token_start)
+		g_data->token_start = new_token;
+	else
+	{
+		aux = g_data->token_start;
+		while (aux->next)
+			aux = aux->next;
+		aux->next = new_token;
+		new_token->prev = aux;
+		new_token->next = NULL;
+	}
+}
+
+void	ft_print_token_list(t_program *g_data)
+{
+	t_token	*aux;
+
+	if(!g_data->token_start)
+		return ;
+	aux = g_data->token_start;
+	while (aux)
+	{
+		printf("%s\n", aux->cmd);
+		aux = aux->next;
+	}
+}
+
+void	ft_free_token_list(t_program *g_data, t_token *token, int start)
+{
+	if (!token)
+		return ;
+	if (token->next)
+		ft_free_token_list(g_data, token->next, 0);
+	else if(token->prev)
+		token->prev->next = NULL;
+	token->prev = NULL;
+	token->next = NULL;
+	if (start == 1)
+		g_data->token_start = NULL;
+	free(token);
+}
