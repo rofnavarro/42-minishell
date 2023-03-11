d409b2f5389cedfa67ab9fb4cbbdced59b2a0103
rofnavarro <rofnavarro@gmail.com>
2023-03-10 21:25:58 -0300

    pwd fix / cd builtin not working no leaks

--- utils/ft_builtin_cd.c
+++ utils/ft_builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 20:12:43 by rferrero          #+#    #+#             */
-/*   Updated: 2023/03/10 20:55:29 by rferrero         ###   ########.fr       */
+/*   Updated: 2023/03/10 21:23:52 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	ft_cd(char *str, t_program *g_data)
 			tmp = tmp->next;
 		aux = ft_substr(tmp->env_line, 4, ft_strlen(tmp->env_line));
 		tmp->next->env_line = ft_strjoin("OLDPWD=", aux);
-		printf("%s\n", tmp->next->env_line);
-		free(aux);
+		// printf("%s\n", tmp->next->env_line);
 	}
+	free(aux);
 }
