ec986d6a2f3ac5fb5d826bf81dcf0b6b26b58dc4
rofnavarro <rofnavarro@gmail.com>
2023-03-24 04:54:58 +0100

    env var change fix / 1 leak 6 bytes malloc substr

--- utils/ft_builtin_env.c
+++ utils/ft_builtin_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_builtin_env.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rferrero <rferrero@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 17:25:37 by rferrero          #+#    #+#             */
-/*   Updated: 2023/03/23 18:31:21 by rferrero         ###   ########.fr       */
+/*   Updated: 2023/03/24 04:52:10 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,29 @@ static int	var_exist(char *var, t_program *g_data, int size)
 
 static void	replace_var_env(char *var, t_program *g_data)
 {
-	printf("\n\n replace \n\n");
+	char	**var_token;
+	char	**tmp;
+	char	*var_tmp;
+	int		i;
+	int		j;
+
+	var_tmp = ft_substr(var, 7, ft_strlen(var) - 7);
+	var_token = ft_split(var_tmp, '=');
+	i = 0;
+	while (g_data->env[i])
+	{
+		tmp = ft_split(g_data->env[i], '=');
+		if (ft_strncmp(var_token[0], tmp[0], ft_strlen(var_token[0]) + 1) == 0)
+		{
+			g_data->env[i] = ft_strdup(var_tmp);
+			ft_free_matrix(tmp);
+			break ;
+		}
+		ft_free_matrix(tmp);
+		i++;
+	}
+	free(var_tmp);
+	ft_free_matrix(var_token);
 }
 
 void	ft_add_var_env(char *new_variable, t_program *g_data)
@@ -61,8 +83,8 @@ void	ft_add_var_env(char *new_variable, t_program *g_data)
 	i = -1;
 	while (g_data->env[++i])
 		new_env[i] = ft_strdup(g_data->env[i]);
+	new_env[i] = ft_substr(new_variable, 7, ft_strlen(new_variable) - 7);
 	ft_free_matrix(g_data->env);
-	new_env[i] = ft_substr(new_variable, 7, ft_strlen(new_variable) - 6);
 	g_data->env = new_env;
 }
 
