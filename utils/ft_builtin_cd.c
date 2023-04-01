7d22914af70d8c44f597b0db587b9584a7a6b417
rofnavarro <rofnavarro@gmail.com>
2023-03-31 21:48:11 -0300

    cd builtin . and .. fix / no leaks

--- utils/ft_builtin_cd.c
+++ utils/ft_builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 20:12:43 by rferrero          #+#    #+#             */
-/*   Updated: 2023/03/31 20:56:51 by rferrero         ###   ########.fr       */
+/*   Updated: 2023/03/31 21:44:45 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,43 @@
 static void	ft_cd_back(char *aux)
 {
 	char	*buff;
-	char	*buff2;
 	char	*path;
-	char	*old_path;
 
 	buff = NULL;
-	if ((ft_strncmp(aux, "..", 2) == 0) && (ft_strlen(aux) == ft_strlen("..")))
-	{
-		old_path = getcwd(buff, 0);
-		chdir("..");
-		path = getcwd(buff2, 0);
-	}
-	printf("\n\n%s\n\n", old_path);
-	printf("\n\n%s\n\n", path);
-	free(buff);
-	free(buff2);
+	chdir("..");
+	path = getcwd(buff, 0);
+	ft_add_var_env(path);
 	free(path);
-	free(old_path);
+	free(buff);
 }
 
 static void	ft_cd_stay(char *aux)
 {
 	char	*buff;
-	char	*buff2;
 	char	*path;
-	char	*old_path;
 
 	buff = NULL;
-	buff2 = NULL;
-	if ((ft_strncmp(aux, ".", 1) == 0) && (ft_strlen(aux) == ft_strlen(".")))
-	{
-		old_path = getcwd(buff, 0);
-		path = getcwd(buff2, 0);
-	}
-	printf("\n\n%s\n\n", old_path);
-	printf("\n\n%s\n\n", path);
-	free(buff);
-	free(buff2);
+	path = getcwd(buff, 0);
+	ft_add_var_env(path);
 	free(path);
-	free(old_path);
+	free(buff);
 }
 
 void	ft_cd(char *variable)
 {
 	char	*aux;
+	char	*buff;
+	char	*old_path;
 
+	buff = NULL;
 	aux = ft_substr(variable, 3, ft_strlen(variable) - 3);
-	ft_cd_back(aux);
-	ft_cd_stay(aux);
+	old_path = getcwd(buff, 0);
+	if ((ft_strncmp(aux, "..", 2) == 0) && (ft_strlen(aux) == ft_strlen("..")))
+		ft_cd_back(aux);
+	else if ((ft_strncmp(aux, ".", 1) == 0) && (ft_strlen(aux) == ft_strlen(".")))
+		ft_cd_stay(aux);
+	printf("\n\n%s\n\n", old_path);
 	free(aux);
+	free(old_path);
+	free(buff);
 }
