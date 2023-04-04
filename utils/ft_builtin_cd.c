1dfaafaa3b6472f2fb08d96518f14bf58ead276f
Rodrigo Ferrero <rofnavarro@gmail.com>
2023-04-04 14:25:20 -0300

    remove unused cd functions / no leaks

--- utils/ft_builtin_cd.c
+++ utils/ft_builtin_cd.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_builtin_cd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: rferrero <rferrero@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 20:12:43 by rferrero          #+#    #+#             */
-/*   Updated: 2023/04/04 00:46:51 by rferrero         ###   ########.fr       */
+/*   Updated: 2023/04/04 14:18:45 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_export_add_env(char *var, char *var_name)
+static void	ft_export_add_env(char *var, char *var_name)
 {
 	char	*tmp;
 	char	*tmp_exp;
@@ -27,7 +27,7 @@ void	ft_export_add_env(char *var, char *var_name)
 	free(exp);
 }
 
-void	ft_check_cd_path(char *input_path)
+static void	ft_check_cd_path(char *input_path)
 {
 	char	*buff;
 	char	*path;
@@ -42,6 +42,35 @@ void	ft_check_cd_path(char *input_path)
 	buff = NULL;
 	path = getcwd(buff, 0);
 	ft_export_add_env(path, "PWD=");
+	free(buff);
+	free(path);
+}
+
+static void	ft_cd_home()
+{
+	char	*buff;
+	char	**home;
+	char	*path;
+	int		i;
+	int		dir;
+
+	i = -1;
+	while (g_data.env[++i])
+	{
+		if (ft_strncmp(g_data.env[i], "HOME=", 5) == 0)
+			break ;
+	}
+	home = ft_split(g_data.env[i], '=');
+	dir = chdir(home[1]);
+	if (dir < 0)
+	{
+		ft_error(errno);
+		return ;
+	}
+	buff = NULL;
+	path = getcwd(buff, 0);
+	ft_export_add_env(path, "PWD=");
+	ft_free_matrix(home);
 	free(buff);
 	free(path);
 }
