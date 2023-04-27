29ed01ea482b14ea8c99bf81dea5fd9b7cd50c1c
rofnavarro <rofnavarro@gmail.com>
2023-04-27 10:56:10 -0300

    +25lines fix quotes_handler \ no leaks

--- utils/ft_quotes_handler.c
+++ utils/ft_quotes_handler.c
@@ -6,11 +6,48 @@
 /*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 14:16:15 by rferrero          #+#    #+#             */
-/*   Updated: 2023/04/27 00:47:29 by rferrero         ###   ########.fr       */
+/*   Updated: 2023/04/27 10:54:41 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+static void	ft_exit_code_handler(int *n, char *tmp, int *m)
+{
+	char	*tmp_var;
+
+	tmp_var = ft_itoa(g_data.exit_code);
+	tmp[*m] = '\0';
+	ft_strlcat(tmp, tmp_var, (*m) + \
+				ft_strlen(tmp_var) + 1);
+	*m = ft_strlen(tmp);
+	free(tmp_var);
+	(*n) += 2;
+}
+
+static void	ft_check_empty_token(t_token *token, int *i)
+{
+	char	**new_cmd;
+	int		lock_value;
+	int		sup;
+
+	if ((token->cmd[*i][0] == 10) && token->cmd[(*i) + 1] != NULL)
+	{
+		lock_value = 0;
+		sup = 0;
+		new_cmd = ft_env_calloc(ft_env_size(token->cmd) + 1);
+		while (token->cmd[lock_value + sup])
+		{
+			if (lock_value + sup == *i)
+				sup = 1;
+			new_cmd[lock_value] = ft_strdup(token->cmd[lock_value + sup]);
+			lock_value++;
+		}
+		ft_free_matrix(token->cmd);
+		token->cmd = new_cmd;
+		(*i)--;
+	}
+}
 
 static void	check_if_var(char *str, int *n, char *tmp, int *m)
 {
@@ -23,9 +60,9 @@ static void	check_if_var(char *str, int *n, char *tmp, int *m)
 	i = 0;
 	while (str[*n] != '\"')
 	{
-		tmp_var[i] = str[*n];
-		i++;
-		(*n)++;
+		if (str[*n] == '\'')
+			break ;
+		tmp_var[i++] = str[(*n)++];
 	}
 	tmp_var[i] = '\0';
 	tmp_value = find_var_value(g_data.env, tmp_var);
@@ -34,27 +71,47 @@ static void	check_if_var(char *str, int *n, char *tmp, int *m)
 	if (tmp_value != NULL)
 	{
 		while (tmp_value[i] != '\0')
-		{
-			tmp[(*m)] = tmp_value[i];
-			(*m)++;
-			i++;
-		}
+			tmp[(*m)++] = tmp_value[i++];
 	}
 	free(tmp_value);
 }
 
+static int	ft_var_handler(char *str, int *n, char **tmp, int *m)
+{
+	int		k;
+	char	*tmp2;
+	char	*tmp_var;
+
+	k = 0;
+	tmp_var = (char *)malloc(sizeof(char) * (ft_strlen(str) - (*n) + 1));
+	tmp_var[ft_strlen(str) - (*n)] = '\0';
+	(*n)++;
+	while (str[(*n)] != '\0')
+		tmp_var[k++] = str[(*n)++];
+	tmp_var[k] = '\0';
+	tmp2 = find_var_value(g_data.env, tmp_var);
+	free(tmp_var);
+	if (tmp2 == NULL)
+	{
+		*m = 1;
+		free(*tmp);
+		free(tmp2);
+		*tmp = ft_strdup("\n");
+		return (TRUE);
+	}
+	*tmp[*m] = '\0';
+	ft_strlcat(*tmp, tmp2, (*m) + ft_strlen(tmp2) + 1);
+	free(tmp2);
+	*m = ft_strlen(*tmp);
+	return (FALSE);
+}
+
 static void	quotes_replace(t_token *token)
 {
-	char	*tmp_var;
 	char	*tmp;
-	char	*tmp2;
-	char	**new_cmd;
 	int		i;
 	int		n;
 	int		m;
-	int		k;
-	int		lock_value;
-	int		sup;
 
 	i = 0;
 	while (token->cmd[i])
@@ -68,11 +125,7 @@ static void	quotes_replace(t_token *token)
 			{
 				n++;
 				while (token->cmd[i][n] != '\'')
-				{
-					tmp[m] = token->cmd[i][n];
-					m++;
-					n++;
-				}
+					tmp[m++] = token->cmd[i][n++];
 				break ;
 			}
 			else if (token->cmd[i][n] == '\"')
@@ -83,36 +136,12 @@ static void	quotes_replace(t_token *token)
 					if (token->cmd[i][n] == '$')
 					{
 						if (token->cmd[i][n + 1] == '?')
-						{
-							tmp_var = ft_itoa(g_data.exit_code);
-							tmp[m] = '\0';
-							ft_strlcat(tmp, tmp_var, m + \
-										ft_strlen(tmp_var) + 1);
-							m = ft_strlen(tmp);
-							free(tmp_var);
-							n += 2;
-						}
+							ft_exit_code_handler(&n, tmp, &m);
 						else
 							check_if_var(token->cmd[i], &n, tmp, &m);
 					}
-					// if (token->cmd[i][n] == '\'')
-					// {
-					// 	tmp[m] = '\'';
-					// 	tmp_var = ft_strtrim(token->cmd[i], '\'');
-					// 	m++;
-					// 	tmp[m] = '\0';
-					// 	ft_strlcat(tmp, tmp_var, m + \
-					// 					ft_strlen(tmp_var) + 1);
-					// 	m = ft_strlen(tmp);
-					// 	free(tmp_var);
-					// 	tmp = (char *)malloc(sizeof(char) * ft_strlen())
-					// }
 					else
-					{
-						tmp[m] = token->cmd[i][n];
-						m++;
-						n++;
-					}
+						tmp[m++] = token->cmd[i][n++];
 				}
 				tmp[m] = '\0';
 				n++;
@@ -124,50 +153,15 @@ static void	quotes_replace(t_token *token)
 					if (token->cmd[i][n] == '$')
 					{
 						if (token->cmd[i][n + 1] == '?')
-						{
-							tmp_var = ft_itoa(g_data.exit_code);
-							tmp[m] = '\0';
-							ft_strlcat(tmp, tmp_var, m + \
-										ft_strlen(tmp_var) + 1);
-							m = ft_strlen(tmp);
-							free(tmp_var);
-							n += 2;
-						}
+							ft_exit_code_handler(&n, tmp, &m);
 						else
 						{
-							k = 0;
-							tmp_var = (char *)malloc(sizeof(char) * (ft_strlen(token->cmd[i]) - n + 1));
-							tmp_var[ft_strlen(token->cmd[i]) - n] = '\0';
-							n++;
-							while (token->cmd[i][n] != '\0')
-							{
-								tmp_var[k] = token->cmd[i][n];
-								n++;
-								k++;
-							}
-							tmp_var[k] = '\0';
-							tmp2 = find_var_value(g_data.env, tmp_var);
-							free(tmp_var);
-							if (tmp2 == NULL)
-							{
-								m = 1;
-								free(tmp);
-								free(tmp2);
-								tmp = ft_strdup("\n");
+							if (ft_var_handler(token->cmd[i], &n, &tmp, &m) == TRUE)
 								break ;
-							}
-							tmp[m] = '\0';
-							ft_strlcat(tmp, tmp2, m + ft_strlen(tmp2) + 1);
-							free(tmp2);
-							m = ft_strlen(tmp);
 						}
 					}
 					else if (token->cmd[i][n] != '\0')
-					{
-						tmp[m] = token->cmd[i][n];
-						m++;
-						n++;
-					}
+						tmp[m++] = token->cmd[i][n++];
 				}
 			}
 			tmp[m] = '\0';
@@ -176,22 +170,7 @@ static void	quotes_replace(t_token *token)
 		free(token->cmd[i]);
 		token->cmd[i] = ft_strdup(tmp);
 		free(tmp);
-		if ((token->cmd[i][0] == 10) && token->cmd[i + 1] != NULL)
-		{
-			lock_value = 0;
-			sup = 0;
-			new_cmd = ft_env_calloc(ft_env_size(token->cmd) + 1);
-			while (token->cmd[lock_value + sup])
-			{
-				if (lock_value  + sup == i)
-					sup = 1;
-				new_cmd[lock_value] = ft_strdup(token->cmd[lock_value + sup]);
-				lock_value++;
-			}
-			ft_free_matrix(token->cmd);
-			token->cmd = new_cmd;
-			i--;
-		}
+		ft_check_empty_token(token, &i);
 		i++;
 	}
 }
