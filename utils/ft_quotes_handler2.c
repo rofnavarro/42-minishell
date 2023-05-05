52426789c8c2faa7f3d930ae39249cb9274f9135
rofnavarro <rofnavarro@gmail.com>
2023-05-04 21:05:37 -0300

    fixed static function place

--- utils/ft_quotes_handler2.c
+++ utils/ft_quotes_handler2.c
@@ -6,7 +6,7 @@
 /*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 10:35:38 by rferrero          #+#    #+#             */
-/*   Updated: 2023/05/04 21:01:59 by rferrero         ###   ########.fr       */
+/*   Updated: 2023/05/04 21:05:18 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,13 @@ void	ft_check_empty_token(t_token *token, int *i)
 	}
 }
 
+static int	ft_break_chars(char c)
+{
+	if (c == '\'' || c > 96 || c == ' ' || c == '}')
+		return (TRUE);
+	return (FALSE);
+}
+
 void	check_if_var(char *str, int *n, char *tmp, int *m)
 {
 	char	*tmp_var;
@@ -71,11 +78,4 @@ void	check_if_var(char *str, int *n, char *tmp, int *m)
 	else
 		tmp[(*m)] = '\0';
 	free(tmp_value);
-}
-
-static int	ft_break_chars(char c)
-{
-	if (c == '\'' || c > 96 || c == ' ' || c == '}')
-		return (TRUE);
-	return (FALSE);
 }
